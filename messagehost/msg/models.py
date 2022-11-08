0c395626e69b380fb6f7a13105af506200411c9b
ll213 <ll213@st-andrews.ac.uk>
2022-11-08 14:07:05 +0000

    add unread_messages model

--- messagehost/msg/models.py
+++ messagehost/msg/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class unread_messages(models.Model):
+    certificate = models.CharField(max_length=256)
+    payload = models.CharField(max_length=2048)
+    receiver = models.CharField(max_length=256)
+
+
